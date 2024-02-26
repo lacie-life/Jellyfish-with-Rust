6bbfe2f9470f0f597cf2ca53d6107e0f67f35a7f
lacie-life <00sao00ios00@gmail.com>
2024-02-27 00:25:15 +0900

    [Rust update]

---
+++ Rust-Basic/other_tech/src/main.rs
@@ -0,0 +1,23 @@
+#![allow(dead_code)]        
+
+mod demo_box1;
+mod demo_box2;
+mod demo_box3;
+mod demo_rc;
+mod demo_unsafe_code;
+mod demo_language_integration;
+
+fn main() {
+
+    // Uncomment one of these statements, to run a demo...
+
+    // demo_box1::do_it();
+    // demo_box2::do_it();
+    // demo_box3::do_it();
+    // demo_rc::do_it();
+    // demo_unsafe_code::do_it();
+    // demo_language_integration::do_it();
+}
+
+
+
